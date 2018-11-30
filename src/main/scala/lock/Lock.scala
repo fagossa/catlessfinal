e2a9ddd49cd2e4e2dc0ff4575ed22da33b7907f9
fagossa <fagossa@gmail.com>
2018-11-30 10:18:19 +0100

    Refactoring

--- src/main/scala/lock/Lock.scala
+++ src/main/scala/lock/Lock.scala
@@ -1,13 +1,14 @@
 package lock
 
-case class LockId(value: String) extends AnyVal
-case class Lock(id: LockId, price: BigDecimal)
+import java.util.UUID
+case class LockId(value: UUID) extends AnyVal
+case class Lock(id: LockId, open: Boolean)
 
-trait LockRepository[F[_]] {
+trait LockRepositoryAlg[F[_]] {
   def findAll: F[List[Lock]]
   def find(id: LockId): F[Option[Lock]]
   def save(lock: Lock): F[Unit]
-  def remove(id: LockId): F[Unit]
+  def disable(id: LockId): F[Unit]
 }
 
 import doobie.implicits._
@@ -15,14 +16,30 @@ import doobie.util.transactor.Transactor
 import cats.effect.Sync
 
 class LockPostgreRepository[F[_]](xa: Transactor[F])
-                                 (implicit F: Sync[F]) extends LockRepository[F] {
+                                 (implicit F: Sync[F]) extends LockRepositoryAlg[F] {
 
-  override def findAll: F[List[Lock]] = sql"select id, price from locks"
+  override def findAll: F[List[Lock]] = /*sql"select id, open from locks"
     .query[Lock]
     .to[List]
-    .transact(xa)
+    .transact(xa)*/
+  ???
 
+  // TODO: implement!!!
   override def find(id: LockId): F[Option[Lock]] = F.pure(None)
   override def save(lock: Lock): F[Unit] = F.unit
-  override def remove(id: LockId): F[Unit] = F.unit
+  override def disable(id: LockId): F[Unit] = F.unit
+}
+
+class BikeRenting[F[_]](lockRepo: LockRepositoryAlg[F])(implicit F: Sync[F]) {
+  def rentBike(lockId: LockId): F[Either[String, Unit]] = {
+    F.flatMap(lockRepo.find(lockId)){
+      case Some(lock) =>
+        val updatedLock: Lock = lock
+        F.flatMap(lockRepo.save(updatedLock)) { _ =>
+          F.pure(Right(()))
+        }
+      case None      =>
+        F.pure(Left("Lock not found"))
+    }
+  }
 }
--- src/test/scala/lock/LockMemRepository.scala
+++ src/test/scala/lock/LockMemRepository.scala
@@ -1,15 +1,20 @@
 package lock
 
+import java.util.UUID
+
 import cats.Id
 import scala.collection.mutable.{Map => MutableMap}
 
-object LockMemRepository extends LockRepository[Id] {
-  private val mem = MutableMap.empty[String, Lock]
+object LockMemRepository extends LockRepositoryAlg[Id] {
+  private val mem = MutableMap.empty[UUID, Lock]
 
   override def findAll: List[Lock] = mem.headOption.map(_._2).toList
+
   override def find(id: LockId): Id[Option[Lock]] = mem.get(id.value)
-  override def save(item: Lock): Id[Unit] = mem.update(item.id.value, item)
-  override def remove(id: LockId): Id[Unit] = {
+
+  override def save(lock: Lock): Id[Unit] = mem.update(lock.id.value, lock)
+
+  override def disable(id: LockId): Id[Unit] = {
     mem.remove(id.value)
     ()
   }
